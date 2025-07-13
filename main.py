4420f7ef85c974f9ee31d3db9d3fb980e80df92f
shiyuqi <shiyuqi129@gmail.com>
2025-07-13 23:26:40 +0800

    Add encoding functions
    Rotor can now be initialized with notches and initial position

--- main.py
+++ main.py
@@ -3,17 +3,36 @@ class Mapping:
         '''Wiring should be every number from 0-n once in any order. 
         Warning: There is no filter for invalid wiring'''
         self.wiring=wiring[:]
-    def encode(self,letter_index: int) -> int:
-        '''Given a number, return the result of the mapping according to the wiring. Return None if input is out of range'''
+    def _encode(self,letter_index: int) -> int:
+        '''Given a number, return the result of the encoding according to the wiring. Return None if input is out of range'''
         if letter_index>len(self.wiring) or letter_index<0:
             return None
         return self.wiring[letter_index]
+    def _decode(self,letter_index: int) -> int:
+        '''Given a number, return the result of the decoding mapping according to the wiring. Return None if input is out of range'''
+        if letter_index>len(self.wiring) or letter_index<0:
+            return None
+        return self.wiring.index(letter_index)
 class Rotor (Mapping):
+    def __init__(self,wiring: list[int],notches: list[int] =[],initial_position=0):
+        '''Initialize a rotor is the given wiring with a list of notch position and set it at an initial position.
+        Warning: No check for invalid input'''
+        self.position=initial_position
+        self.notches=notches
+        super().__init__(wiring[initial_position:]+wiring[:initial_position])
     def rotate(self):
         '''Rotate left by 1 space'''
         self.wiring=self.wiring[1:]+self.wiring[:1]
+    def forward(self,letter_index: int) -> int:
+        '''Given a input from the direction of the entry wheel, return the output of the rotor'''
+        return super()._encode(letter_index)
+    def backward(self,letter_index: int) -> int:
+        '''Given a input from the direction of the reflector, return the output of the rotor'''
+        return super()._decode(letter_index)
 class Reflector (Mapping):
-    pass
+    def reflect(self,letter_index: int) -> int:
+        '''Given a input, return the output of the reflector'''
+        return super()._encode(letter_index)
 class Plugboard (Mapping):
     def __init__(self,connections: list[list[int]]=[],size=26):
         '''Initialize the plugboard with a list of connections.
@@ -23,6 +42,9 @@ class Plugboard (Mapping):
             wiring[i]=j
             wiring[j]=i
         super().__init__(wiring)
+    def swap(self,letter_index):
+        '''Given a input, return the output of the plugboard'''
+        return super()._encode(letter_index)
     def add_connection(self,i,j=None):
         '''Add connection between i and j. Input could also be a list or a tuple
         Warning: There is no filter for invalid or overlapping connections'''
