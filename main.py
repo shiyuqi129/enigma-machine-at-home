dbeb0695906d48b61fc4cdbc51f25bbcc62e4919
shiyuqi <shiyuqi129@gmail.com>
2025-07-14 23:51:17 +0800

    Add some error detection

--- main.py
+++ main.py
@@ -3,63 +3,90 @@ class Mapping:
         '''Wiring should be every number from 0-n once in any order. 
         Warning: There is no filter for invalid wiring'''
         self.wiring=wiring[:]
-    def _encode(self,letter_index: int) -> int:
+        self.size=len(wiring)
+    def _encode(self,letter_index: int) -> int | None:
         '''Given a number, return the result of the encoding according to the wiring. Return None if input is out of range'''
         if letter_index>len(self.wiring) or letter_index<0:
             return None
         return self.wiring[letter_index]
-    def _decode(self,letter_index: int) -> int:
+    def _decode(self,letter_index: int) -> int | None:
         '''Given a number, return the result of the decoding mapping according to the wiring. Return None if input is out of range'''
         if letter_index>len(self.wiring) or letter_index<0:
             return None
         return self.wiring.index(letter_index)
 class Rotor (Mapping):
-    def __init__(self,wiring: list[int],notches: list[int] =[],initial_position=0):
+    def __init__(self,wiring: list[int],notches: list[int] =[],initial_position: int =0):
         '''Initialize a rotor is the given wiring with a list of notch position and set it at an initial position.
         Warning: No check for invalid input'''
         self.position=initial_position
         self.notches=notches
         super().__init__(wiring[initial_position:]+wiring[:initial_position])
-    def rotate(self):
+    def rotate(self) -> None:
         '''Rotate left by 1 space'''
         self.wiring=self.wiring[1:]+self.wiring[:1]
-    def forward(self,letter_index: int) -> int:
+    def forward(self,letter_index: int) -> int | None:
         '''Given a input from the direction of the entry wheel, return the output of the rotor'''
         return super()._encode(letter_index)
-    def backward(self,letter_index: int) -> int:
+    def backward(self,letter_index: int) -> int | None:
         '''Given a input from the direction of the reflector, return the output of the rotor'''
         return super()._decode(letter_index)
 class Reflector (Mapping):
-    def reflect(self,letter_index: int) -> int:
+    def __init__(self,wiring: list[int]):
+        if not isinstance(wiring,list):
+            raise TypeError("wiring must be a list")
+        if any(not isinstance(x,int) for x in wiring):
+            raise TypeError("wiring must be a list of integer")
+        if any(x<0 or x>=len(wiring) for x in wiring):
+            raise ValueError("Integers in wiring must be non-negative and less than the length of wiring")
+        if any(wiring[x]!=wiring.index(x) for x in wiring):
+            raise ValueError("The wiring lead to a reflector that is not reflective")
+        super().__init__(wiring)
+    def reflect(self,letter_index: int) -> int | None:
         '''Given a input, return the output of the reflector'''
         return super()._encode(letter_index)
 class Plugboard (Mapping):
-    def __init__(self,connections: list[list[int]]=[],size=26):
-        '''Initialize the plugboard with a list of connections.
-        Warning: There is no filter for invalid connections'''
+    def __init__(self,connections: list[list[int]]=[],size: int=26):
+        '''Initialize the plugboard with a list of connections.'''
+        if not isinstance(size,int):
+            raise TypeError("size must be an integer")
+        if not isinstance(connections,list):
+            raise TypeError("connection must be a list")
+        if size<=0:
+            raise ValueError("size must be positive integer")
         wiring=[i for i in range(size)]
-        for i,j in connections:
+        for connection in connections:
+            if not isinstance(connection,list):
+                raise TypeError("Each connection must be a list containing exatly two integer")
+            if len(connection)!=2:
+                raise ValueError("Each connection must be a list containing exatly two integer")
+            i,j=connection
+            if not (isinstance(i,int) and isinstance(j,int)):
+                raise TypeError("Each connection must be a list containing exatly two integer")
+            if i<0 or i>=size or j<0 or j>=size:
+                raise ValueError("Connection contain index out of range")
+            if wiring[i]!=i or wiring[j]!=j or i==j:
+                raise ValueError("Repeated or conflicted plugboard configuration")
             wiring[i]=j
             wiring[j]=i
         super().__init__(wiring)
-    def swap(self,letter_index):
+    def swap(self,letter_index: int) -> int | None:
         '''Given a input, return the output of the plugboard'''
         return super()._encode(letter_index)
-    def add_connection(self,i,j=None):
+    def add_connection(self,i,j=None) -> None:
         '''Add connection between i and j. Input could also be a list or a tuple
         Warning: There is no filter for invalid or overlapping connections'''
         if j==None:
             i,j=i
         self.wiring[i]=j
         self.wiring[j]=i
-    def remove_connection(self,i,j=None):
+    def remove_connection(self,i,j=None) -> None:
         '''Remove connection between i and j. Input could also be a list or a tuple
         Warning: There is no filter for invalid or non-existing connections'''
         if j==None:
             i,j=i
         self.wiring[i]=i
         self.wiring[j]=j
-    def remove_all_connection(self):
+    def remove_all_connection(self) -> None:
         '''Remove every connections on the plugboard'''
         for i in range(len(self.wiring)):
             self.wiring[i]=i
