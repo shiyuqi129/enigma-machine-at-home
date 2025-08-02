0a003ff1eca61dfcb61b62e302d3dbdedb417d31
shiyuqi <shiyuqi129@gmail.com>
2025-08-02 23:34:37 +0800

    Add write_positions_string()
    read_wiring_string() has been renamed to read_positions_string()

--- wirings.py
+++ wirings.py
@@ -26,12 +26,23 @@ REFLECTOR_WIRING_STRINGS={
     "C"     :"FVPJIAOYEDRZXWGCTKUQSBNMHL"
 }
 
-def read_wiring_string(wiring:str) -> list[int]:
-    '''Given a wiring written in string of A to Z, convert it to a list of integer from 0 to 25'''
-    if not isinstance(wiring,str):
+def read_position_string(positions:str) -> list[int]:
+    '''Given positions written in string of A to Z, convert it to a list of integer from 0 to 25'''
+    if not isinstance(positions,str):
         raise TypeError("Input must be a string")
-    return [ord(c.upper())-ord('A') for c in wiring if c.isalpha()]
+    return [ord(c.upper())-ord("A") for c in positions if c.isalpha()]
 
-ROTOR_WIRINGS={name:read_wiring_string(wiring_string) for name,wiring_string in ROTOR_WIRING_STRINGS.items()}
-ROTOR_NOTCHES={name:read_wiring_string(notches_string) for name,notches_string in ROTOR_NOTCH_STRINGS.items()}
-REFLECTOR_WIRINGS={name:read_wiring_string(wiring_string) for name,wiring_string in REFLECTOR_WIRING_STRINGS.items()}
+def write_position_string(positions:list[int]) -> str:
+    '''Given a list of integer from 0 to 25, convert it to positions written in string of A to Z'''
+    if not isinstance(positions,list):
+        raise TypeError("Input must be a list of integers")
+    if not all(isinstance(num,int) for num in positions):
+        raise TypeError("Input must be a list of integers")
+    if not all(26>num>=0 for num in positions):
+        raise ValueError("Some of the position are out of range. Input should be in 0~25")
+    wiring_letters=[chr(num+ord("A")) for num in positions]
+    return "".join(wiring_letters)
+
+ROTOR_WIRINGS={name:read_position_string(wiring_string) for name,wiring_string in ROTOR_WIRING_STRINGS.items()}
+ROTOR_NOTCHES={name:read_position_string(notches_string) for name,notches_string in ROTOR_NOTCH_STRINGS.items()}
+REFLECTOR_WIRINGS={name:read_position_string(wiring_string) for name,wiring_string in REFLECTOR_WIRING_STRINGS.items()}
